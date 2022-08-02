88038f524314d7b7c97cf457bafb0f1f180df29b
matth <mdhutchinson@upei.ca>
2022-08-02 13:17:39 -0300

    Comments for everything

--- LatencyTester.cpp
+++ LatencyTester.cpp
@@ -1,42 +1,47 @@
 
+//Initialize variables
 int currentLight = 0;
 int pastLight = 0;
 int buttonTime = 0;
 int lightTime = 0;
 int button = 0;
+
 void setup()
 {
-	pinMode(A0, INPUT);
-    pinMode(2, INPUT_PULLUP);
+    //setup inputs.
+	pinMode(A0, INPUT); //A0 is for the photosensor
+    pinMode(2, INPUT_PULLUP); //pin 2 is for the button
     pinMode(LED_BUILTIN, OUTPUT);
-
     Serial.begin(9600);
 }
 
 void loop()
 {
+    // Read read the current light levels from the photosensor
     pastLight = analogRead(A0);
     currentLight = pastLight;
-    //Serial.println(pastLight);
+    // Read button input
     button = digitalRead(2);
-    //Serial.println(button);
     digitalWrite(LED_BUILTIN, HIGH);
-    //Serial.println(button);
+    // if the button is pressed
     if(button){
+        //count the time at button press
         buttonTime = millis();
-        //Serial.println("IN IF");
+        //loop until the current light is greater than the past light + 50 (The screen flashes)
         while(pastLight + 50 > currentLight){
+            // collect the current time
             lightTime = millis();
             currentLight = analogRead(A0);
+            // Simple timeout. If it takes more than 5 seconds for light to flash. break loop
             if((lightTime - buttonTime) > 5000){
                 Serial.println("TIMEOUT");
                 break;
             }
-            //Serial.println(currentLight);
-        }  
-        //Serial.println("#######");
+        }
+        // Print the time it took for the light to come on subtract the time of the button press to get difference between button input and screen output.
         Serial.println(lightTime - buttonTime);  
         digitalWrite(LED_BUILTIN, LOW);
+        // Wait 1 second to allow the screen to go back to black
         delay(1000);
     }
     
--- Screen_Software/Movement_Flash.py
+++ Screen_Software/Movement_Flash.py
@@ -2,37 +2,46 @@ import pygame
 
 pygame.init()
 
+"""
+This program will run on joystick inputs if a joystick is plugged in. If no joystick is found it will use mouse inputs.
+When movement or button inputs are found it will flash the screen white for 1 second before returning to black.s
+"""
+
+# If there is a joystick, run this code. Otherwise run alternative code for mouse controls.
 if(pygame.joystick.get_count()>0):
+    # get joystick, start event listener, grab focus
     stick = pygame.joystick.Joystick(0)
-    print(stick.get_name())
     pygame.event.pump()
     pygame.display.init()
     pygame.event.set_grab(True)
-
+    # Open window, 800x800 resolution.
     window = pygame.display.set_mode((800,800)) # 0, 0, fullscreen
-
+    # set mouse cursor
     cursor = pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
     pygame.mouse.set_cursor(cursor)
 
-
+    # initialize variables
     prevMouse = (0,0)
     currentMouse = (0, 0)
     deadZone = 0.01
+    # game loop
     going = True
     pygame.time.Clock().tick(90)
     while going:
-
+        # Read from event queue
         event = pygame.event.poll()
+        # uncomment if you want to the screen to react to joystick motion.
+        #
+        # if event.type == pygame.JOYAXISMOTION:
+        #     if stick.get_axis(0) > deadZone or stick.get_axis(0) < -deadZone or stick.get_axis(1) > deadZone or stick.get_axis(1) < -deadZone: #0.1 is the deadzone im applying to deal with jitter
+        #         window.fill((255,255,255))
+        #         pygame.display.flip()
+        #         pygame.time.wait(1000)
+        #
+        #     window.fill((0,0,0))
+        #     pygame.display.flip()
 
-        if event.type == pygame.JOYAXISMOTION:
-            if stick.get_axis(0) > deadZone or stick.get_axis(0) < -deadZone or stick.get_axis(1) > deadZone or stick.get_axis(1) < -deadZone: #0.1 is the deadzone im applying to deal with jitter
-                window.fill((255,255,255))
-                pygame.display.flip()
-                pygame.time.wait(1000)
-
-            window.fill((0,0,0))
-            pygame.display.flip()
-
+        # if there is a joystick button pushed. Flash the screen white (255,255,255) ,wait 1 second, then go back to black (0,0,0)
         if event.type == pygame.JOYBUTTONDOWN:
             window.fill((255,255,255))
             pygame.display.flip()
@@ -40,35 +49,49 @@ if(pygame.joystick.get_count()>0):
             window.fill((0,0,0))
             pygame.display.flip()
 
+        # Process quit button being clicked to end game loop
         if event.type == pygame.QUIT:
             going = False
-
+        # process escape key being pressed to end game loop
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 going = False
 
 else:
-    print("Mouse")
+    #Run the loop for mouse instead of joystick
+    #start event reader and grab focus
     pygame.event.pump()
     pygame.display.init()
     pygame.event.set_grab(True)
-
+    #open window at default resolution 800x800
     window = pygame.display.set_mode((800,800)) # 0, 0, fullscreen
-
+    # set game cursor
     cursor = pygame.cursors.Cursor(pygame.SYSTEM_CURSOR_CROSSHAIR)
     pygame.mouse.set_cursor(cursor)
 
-
+    # Initialize variables
     prevMouse = (0,0)
     currentMouse = (0, 0)
     deadZone = 0.1
+    # game loop
     going = True
     pygame.time.Clock().tick(90)
     while going:
-
+        # start event listener
         event = pygame.event.poll()
 
-        if event.type == pygame.MOUSEMOTION or event.type==pygame.MOUSEBUTTONDOWN:
+        # uncomment if you want to the screen to react to mouse motion.
+        #
+        # if event.type == pygame.MOUSEMOTION:
+        #     window.fill((255,255,255))
+        #     pygame.display.flip()
+        #     pygame.time.wait(1000)
+        #     window.fill((0,0,0))
+        #     pygame.display.flip()
+
+
+        # Listen for mouse button being pressed. If pressed fills screen with white (255,255,255) then wait 1 second and return to black (0,0,0)
+        if event.type==pygame.MOUSEBUTTONDOWN:
 
             window.fill((255,255,255))
             pygame.display.flip()
@@ -76,10 +99,10 @@ else:
 
             window.fill((0,0,0))
             pygame.display.flip()
-
+        # process quit button being clicked and end game loop
         if event.type == pygame.QUIT:
             going = False
-
+        # Process escape button being pressed and end game loop
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 going = False
